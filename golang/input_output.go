43de9a4edf05f451e4ec54359e18e070be91ba82
Arnaud Wanet <andrewwanet9@gmail.com>
2022-12-31 11:13:21 +0100

    added notes on go memory model

---
+++ golang/input_output.go
@@ -0,0 +1,30 @@
+package interfaces 
+
+import (
+	"fmt" 
+	"io" 
+	"os"
+)
+
+// Copy copies data from in to out first directly 
+// then using a buffer. It also writes to stdout
+func Copy(in io.ReadSeeker, out io.Writer) error{
+	// we write to out but also stdout 
+	w := io.MultiWriter(out, os.Stdout)
+	// a standard copy, this can be dangerous if there is 
+	// a lot of data in in 
+	if _, error := io.Copy(w, in); err != nil{
+		return err
+	}
+	in.Seek(0, 0)
+
+	// Buffered write using 64 byte chunks 
+	buf := make([]byte, 64)
+	if _, err := io.CopyBuffer(w, in, buf); err != nil{
+		return err 
+	}
+
+	// lets print a new line 
+	fmt.Println() 
+	return nil 
+}
